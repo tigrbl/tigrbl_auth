a553e7e7b8cddb5b1d2cdbf11b65f4d246fa1512
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-06-22 03:39:13 -0500

    Prune capability cleanup additions

--- pkgs/40-capabilities/tigrbl-authn-credentials/src/tigrbl_authn_credentials/_token_service/__init__.py
+++ pkgs/40-capabilities/tigrbl-authn-credentials/src/tigrbl_authn_credentials/_token_service/__init__.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 from tigrbl_identity_contracts.tokens import InvalidRefreshTokenError, RefreshTokenError, RefreshTokenReuseError
 
 from .coder import InvalidTokenError, JWTCoder
-from .context import (
+from ..session_service import (
     exchange_token_for_context,
     get_token_for_context,
     introspect_token_for_context,
     list_tokens_for_context,
-    parse_token_patch,
     revoke_all_tokens_for_context,
     revoke_token_for_context,
 )
@@ -25,7 +24,6 @@ __all__ = [
     'introspect_token_for_context',
     'issue_persisted_token_pair',
     'list_tokens_for_context',
-    'parse_token_patch',
     'redeem_refresh_token',
     'revoke_all_tokens_for_context',
     'revoke_token_for_context',
